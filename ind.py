560145b34019edb1ae96d66da838bfdbbc0ae247
Stella Kibika <stellakibika@gmail.com>
2021-07-31 21:53:21 +0300

    initial commit

--- ind.py
+++ ind.py
@@ -1,8 +1,10 @@
 """Application entry point."""
+from whitenoise import WhiteNoise
 from apps import init_app
 
 app = init_app()
 
 if __name__ == "__main__":
     app.run(debug=False)
+    app.wsgi_app = WhiteNoise(app.wsgi_app, root='static/')
     # app.run(host="0.0.0.0")
